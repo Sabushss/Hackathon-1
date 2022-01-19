ff1a6154a6afc0461fcfa5696f0ff583ff07880f
Dinesh Krishnan S <71324224+DineshKrish-s@users.noreply.github.com>
2022-01-19 17:18:35 +0530

    Update Health_Insurance.java

--- src/test/java/com/policybazzar/Health_Insurance.java
+++ src/test/java/com/policybazzar/Health_Insurance.java
@@ -13,27 +13,28 @@ import pom.HealthInsurancePom;
 public class Health_Insurance{
 
     WebDriver driver;
-    static WebElement elec;
+    static WebElement elec; //delcaring Webelement elec
     
     public void process(WebDriver driver){
 
         this.driver = driver;
         
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        
+        //creating object to call health insurance POM
         HealthInsurancePom fetch = new HealthInsurancePom();
-        
+        //Creating object to call actions class
         Actions builder = new Actions(driver);
+        //performing action to hover on insurance products
         WebElement elec = driver.findElement(fetch.insurance_product);
         builder.moveToElement(elec).perform();
-       
+        //creating a  list to retrieve the webelements 
         List<WebElement> allLinks = driver.findElements(fetch.all_links);
-        List<String>output=new ArrayList<String>();
-       
+        List<String>output=new ArrayList<String>(); //creating a new list of string data type
+       //Using for loop to fetch the list and store it 
         for(WebElement e:allLinks){              
               output.add(e.getText());                
         }
-        
+        //Printing the list in the console
         System.out.println(output);
 
     }
